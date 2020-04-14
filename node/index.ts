54588aeb5768f98dee5a871d0cf3ca9c600a521a
dhasuda <davihasuda@gmail.com>
2020-04-14 14:09:30 -0300

    Testing :see_no_evil:

--- node/index.ts
+++ node/index.ts
@@ -33,7 +33,6 @@ declare global {
   // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
   type Context = ServiceContext<Clients, State>
 
-  // The shape of our State object found in `ctx.state`. This is used as state bag to communicate between middlewares.
   interface State {
     code: number
   }
